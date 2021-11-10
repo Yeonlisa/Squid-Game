79013e8dcaf1db6245e95090bf0caf2c38b75c49
yeonlisa <tkzw4869@gmail.com>
2021-11-10 18:55:07 +0900

    loading 3D model

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+renderer.setClearColor( 0xb7c3f3, 1 );
+
 const light = new THREE.AmbientLight( 0xffffff );
 scene.add( light );
 
@@ -15,8 +17,24 @@ scene.add( light );
 
 camera.position.z = 5;
 
+const loader = new THREE.GLTFLoader();
+loader.load("../models/scene.gltf", (gltf) => {
+	scene.add( gltf.scene );
+	gltf.scene.scale.set(.4, .4, .4);
+	gltf.scene.position.set(0, -1, 0);
+})
+
 function animate() {
 	renderer.render( scene, camera );
 	requestAnimationFrame( animate );
 }
 animate();
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
