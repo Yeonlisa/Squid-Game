f1ca4e8e26668172e1ef819205411cec9cead362
yeonlisa <tkzw4869@gmail.com>
2021-11-11 14:57:10 +0900

    Creating Track

--- main.js
+++ main.js
@@ -10,10 +10,20 @@ renderer.setClearColor( 0xb7c3f3, 1 );
 const light = new THREE.AmbientLight( 0xffffff );
 scene.add( light );
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+// global variables
+const start_position = 3;
+const end_position = -start_position;
+
+
+function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
+	const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+	const material = new THREE.MeshBasicMaterial( { color: color } );
+	const cube = new THREE.Mesh( geometry, material );
+	cube.position.x = positionX;
+	cube.rotation.y = rotY;
+	scene.add( cube );
+	return cube;
+}
 
 camera.position.z = 5;
 
@@ -36,6 +46,13 @@ class Doll {
 		gsap.to(this.doll.rotation, {y: 0, duration: .45})
 	}
 }
+
+function createTrack() {
+	createCube({w: start_position * 2 + .2, h: 1.5, d: 1}, 0, 0, 0xe5a716).position.z = -1;
+	createCube({w: .2, h: 1.5, d: 1}, start_position, -.35);
+	createCube({w: .2, h: 1.5, d: 1}, end_position, .35);
+}
+createTrack();
 
 let doll = new Doll();
 setTimeout(() => {
